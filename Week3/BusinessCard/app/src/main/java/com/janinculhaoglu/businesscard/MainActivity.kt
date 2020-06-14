8ae6d6886337ed9fac08886349abe609e90adba4
Janin Culhaoglu <janin.culhaoglu@gmail.com>
2020-06-15 01:07:01 +0200

    Implemented the action on experience button / restored text when orientation change

--- Week3/BusinessCard/app/src/main/java/com/janinculhaoglu/businesscard/MainActivity.kt
+++ Week3/BusinessCard/app/src/main/java/com/janinculhaoglu/businesscard/MainActivity.kt
@@ -2,11 +2,63 @@ package com.janinculhaoglu.businesscard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val experienceList = listOf(
+        "Creation of a native iOS app to find recipes by ingredient",
+        "Creation of an app to help you find eco-friendly places nearby",
+        "Creation of an android business card app"
+    )
+
+    private var experienceNumber = 0
+
+    private lateinit var otherExperienceButton: Button
+    private lateinit var experienceTextView: TextView
+
+    companion object {
+        private const val EXPERIENCE_KEY = "EXPERIENCE_KEY"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        otherExperienceButton = findViewById(R.id.newExperienceButton)
+        experienceTextView = findViewById(R.id.experienceTextView)
+
+        otherExperienceButton.setOnClickListener {
+            changeExperienceDisplay()
+        }
+
+        if (savedInstanceState != null) {
+            experienceNumber = savedInstanceState.getInt(EXPERIENCE_KEY)
+            restoreExperience()
+        } else {
+            resetExperience()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(EXPERIENCE_KEY, experienceNumber)
+    }
+
+    private fun changeExperienceDisplay() {
+        if (experienceNumber == experienceList.lastIndex) {
+            experienceNumber = 0
+        } else {
+            experienceNumber += 1
+        }
+        experienceTextView.text = experienceList[experienceNumber]
+    }
+
+    private fun restoreExperience() {
+        experienceTextView.text = experienceList[experienceNumber]
+    }
+
+    private fun resetExperience() {
+        experienceTextView.text = experienceList.first()
     }
 }

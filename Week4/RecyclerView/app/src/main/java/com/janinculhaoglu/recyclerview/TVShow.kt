018e380ee727af562aed6af213fb0899e75a7727
Janin Culhaoglu <janin.culhaoglu@gmail.com>
2020-06-20 18:52:08 +0200

    Created TVShow data class and TVShow objects in Utility

---
+++ Week4/RecyclerView/app/src/main/java/com/janinculhaoglu/recyclerview/TVShow.kt
@@ -0,0 +1,9 @@
+package com.janinculhaoglu.recyclerview
+
+data class TVShow(
+    val id: Int,
+    val title: String,
+    val releaseDate: String,
+    val summary: String,
+    val poster: Int
+)
---
+++ Week4/RecyclerView/app/src/main/java/com/janinculhaoglu/recyclerview/Utility.kt
@@ -0,0 +1,69 @@
+package com.janinculhaoglu.recyclerview
+
+val tvshowsList = setOf(
+    TVShow(
+        id = 1,
+        title = "Malcolm in the middle",
+        releaseDate = "2000-2006",
+        summary = "A gifted young teen tries to survive life with his dimwitted, dysfunctional" +
+                " family.",
+        poster = R.drawable.malcolm
+    ),
+
+    TVShow(
+        id = 2,
+        title = "Friends",
+        releaseDate = "1994–2004",
+        summary = " Follows the personal and professional lives of six friends living in " +
+                "Manhattan.",
+        poster = R.drawable.friends
+    ),
+
+    TVShow(
+        id = 3,
+        title = "The Simpsons",
+        releaseDate = "1989",
+        summary = "The satiric adventures of a working-class family in the misfit city of " +
+                "Springfield.",
+        poster = R.drawable.the_simpsons
+    ),
+
+    TVShow(
+        id = 4,
+        title = "The Big Bang Theory",
+        releaseDate = "2007-2019",
+        summary = " A woman who moves into an apartment across the hall from two brilliant but " +
+                "socially awkward physicists shows them how little they know about life outside " +
+                "of the laboratory.",
+        poster = R.drawable.the_big_bang_theory
+    ),
+
+    TVShow(
+        id = 5,
+        title = "Community",
+        releaseDate = "2009-2015",
+        summary = " A suspended lawyer is forced to enroll in a community college with an " +
+                "eccentric staff and student body.",
+        poster = R.drawable.community
+    ),
+
+    TVShow(
+        id = 6,
+        title = "Brooklyn Nine-Nine",
+        releaseDate = "2013",
+        summary = "Brooklyn Nine-Nine follows the exploits of hilarious Det. Jake Peralta and " +
+                "his diverse lovable colleagues as they police the NYPD's 99th Precinct.",
+        poster = R.drawable.brooklyn_nine_nine
+    ),
+
+    TVShow(
+        id = 7,
+        title = "The Middle",
+        releaseDate = "2009-2018",
+        summary = " The daily mishaps of a married woman and her semi-dysfunctional family and " +
+                "their attempts to survive life in general in the town of Orson, Indiana.",
+        poster = R.drawable.the_middle
+    )
+)
+
+
